8dbf5855d79af980cca470fc73bad2a19f3f18c4
Samir Ayoub <123samir@gmail.com>
2020-10-14 13:45:39 -0300

    feat: implementando tooltip

--- projects/ui/src/lib/components/po-chart/directives/po-chart-tooltip.directive.ts
+++ projects/ui/src/lib/components/po-chart/directives/po-chart-tooltip.directive.ts
@@ -3,6 +3,8 @@ import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular
 import { PoChartTooltipBaseDirective } from './po-chart-tooltip-base.directive';
 import { PoTooltipControlPositionService } from '../../../directives/po-tooltip/po-tooltip-control-position.service';
 
+const TOP_AREA = 48;
+
 @Directive({
   selector: '[p-chart-tooltip]',
   providers: [PoTooltipControlPositionService]
@@ -40,33 +42,46 @@ export class PoChartTooltipDirective extends PoChartTooltipBaseDirective impleme
     });
   }
 
-  create() {
+  createSvgTextElement() {
     this.tooltipText = this.renderer.createElement('svg:text', 'svg');
-    this.tooltipText.textContent = 'samir teste';
+    this.tooltipText.textContent = 'samir tesdsfsfsdfsfsfte';
     this.renderer.addClass(this.tooltipText, 'po-chart-tooltip-text');
-    this.renderer.setAttribute(this.tooltipText, 'x', this.tooltipElement['x']);
-    this.renderer.setAttribute(this.tooltipText, 'y', this.tooltipElement['y']);
-
-    const svgPointsGroup = this.renderer.parentNode(this.elementRef.nativeElement);
-    this.renderer.appendChild(svgPointsGroup, this.tooltipText);
-
-    this.createRect(svgPointsGroup);
+    this.renderer.setAttribute(this.tooltipText, 'x', (this.tooltipElement['x'] - 10).toString());
+    this.renderer.setAttribute(this.tooltipText, 'y', (this.tooltipElement['y'] - TOP_AREA).toString());
   }
 
-  createRect(svgPointsGroup) {
+  create() {
+    this.createSvgTextElement();
+
+    const svgPointsGroup = this.renderer.parentNode(this.elementRef.nativeElement);
+
+    this.renderer.appendChild(svgPointsGroup, this.tooltipText);
     const SVGRect = this.tooltipText.getBBox();
+    // this.renderer.removeChild(svgPointsGroup, this.tooltipText)
+    console.log('svgRect', SVGRect);
+
+    this.createRect(SVGRect);
+  }
+
+  createRect(SVGRect) {
+    const svgPointsGroup = this.renderer.parentNode(this.elementRef.nativeElement);
+
+    const group = this.renderer.createElement('svg:g', 'svg');
 
     this.tooltip = this.renderer.createElement('svg:rect', 'svg');
     this.renderer.addClass(this.tooltip, 'po-chart-tooltip');
     this.renderer.setAttribute(this.tooltip, 'width', SVGRect.width);
     this.renderer.setAttribute(this.tooltip, 'height', SVGRect.height);
-    this.renderer.setAttribute(this.tooltip, 'x', this.tooltipElement['x']);
-    this.renderer.setAttribute(this.tooltip, 'y', this.tooltipElement['y']);
+    this.renderer.setAttribute(this.tooltip, 'x', (this.tooltipElement['x'] - 10).toString());
+    this.renderer.setAttribute(this.tooltip, 'y', (this.tooltipElement['y'] - TOP_AREA - 20).toString());
     this.renderer.setAttribute(this.tooltip, 'fill', 'yellow');
 
-    this.renderer.appendChild(svgPointsGroup, this.tooltip);
+    this.renderer.appendChild(group, this.tooltip);
+    this.renderer.appendChild(group, this.tooltipText);
 
-    this.renderer.insertBefore(svgPointsGroup, this.tooltip, this.tooltipText);
+    this.renderer.appendChild(svgPointsGroup, group);
+
+    // this.renderer.insertBefore(svgPointsGroup, this.tooltip, this.tooltipText);
 
     // this.renderer.appendChild(svgPointsGroup, this.tooltipText);
 
